e7d471c85ad2f383b014ede951f19fa1984bb0ca
Thor Anderson <ta.anderson@gmail.com>
2020-10-29 11:13:52 -0600

    Added functions and some genius level

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -19,27 +19,45 @@ otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
 const maleCharacters = people.filter((person) => person.gender === 'male')
-console.log(maleCharacters)
 
-maleButton.addEventListener('click', (event) => {
-  maleCharacters.forEach((element) => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/10.jpg`
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = `Luke Skywalker`
+const femaleCharacters = people.filter((person) => person.gender === 'female')
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 
-    mainContent.appendChild(charFigure)
-  })
-})
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 
-let theUrl = "https://swapi.co/api/people/2/"
-
-function getLastNumber(url) {
-    console.log(url)
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
 }
 
-getLastNumber(theUrl)
+// let theUrl = "https://swapi.co/api/people/2/"
+// let theUrl2 = "https://swapi.co/api/people/12/"
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -1,5 +1,4 @@
-import { films } from './data/films.js'
-import { people } from './data/people.js'
+import { films } from '../data/films.js'
 
 const main = document.querySelector('main')
 
