937dcf7d6a95bad549a8dceb3954d1d7ee137fba
Thor Anderson <ta.anderson@gmail.com>
2020-11-05 11:26:34 -0700

    Refactored to Utils file and added Bulma dialog

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -1,4 +1,5 @@
 import { people } from '../data/people.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const mainContent = document.querySelector('#main')
 
@@ -61,17 +62,3 @@ function populateDOM(characters) {
 // let theUrl = "https://swapi.co/api/people/2/"
 // let theUrl2 = "https://swapi.co/api/people/12/"
 
-function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    let start = end - 2
-    if (url.charAt(start) === '/') {
-        start++
-    }
-    return url.slice(start, end)
-}
-
-function removeChildren(container) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-}
--- starships/main.js
+++ starships/main.js
@@ -1,8 +1,21 @@
 import { starships } from '../data/starships.js'
+import { removeChildren, getLastNumber } from '../utils/index.js'
 
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.main')
+
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
 
 function populateNav(starships) {
     starships.forEach(starship => {
@@ -24,12 +37,19 @@ function populateNav(starships) {
 }
 
 function populateShipView(shipData) {
-    console.log(shipData)
+    removeChildren(shipView)
     // use createElement to make new img elements
+    let shipImage = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
     // set their src to the url below
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
     // make shipNum act like charNum from the characters page
-    //https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg
-    // append the image to the shipView element
+    shipImage.addEventListener('error', () => {
+        shipImage.hidden = true
+        dialog.classList.toggle("is-active")
+    }) // genius level 965!!
+    
+    shipView.appendChild(shipImage)
 }
 
 populateNav(starships)
