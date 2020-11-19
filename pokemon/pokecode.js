f5d0faccf7d2ecd544c3f6003b3e72d3d2d52520
Thor Anderson <ta.anderson@gmail.com>
2020-11-19 11:12:48 -0700

    Got card flip working

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -15,7 +15,6 @@ function loadPage() {
         (async (data) => {
             for (const pokemon of data.results) {
                 await getAPIData(pokemon.url).then((pokeData) => {
-                    console.log(pokeData)
                     populatePokeCard(pokeData)
                 })
             }
@@ -23,29 +22,64 @@ function loadPage() {
 }
 
 const pokemonGrid = document.querySelector('.pokemonGrid')
+const loadButton = document.querySelector('button')
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+    loadButton.disabled = true
+})
+
+/* mudsDaleButton.addEventListener('click', () => {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/750`).then
+        (async (data) => {
+            let mudMoves = document.createElement('ul')
+            data.moves.forEach(move => {
+                console.log(move.move.name)
+                let moveItem = document.createElement('li')
+                moveItem.textContent = move.move.name
+                mudMoves.appendChild(moveItem)
+            })
+            let mudImage = document.createElement('img')
+            mudImage.src = `../images/pokemon/750.png`
+            pokemonGrid.appendChild(mudMoves)
+            pokemonGrid.appendChild(mudImage)
+    })
+}) */
 
 function populatePokeCard(pokemon) {
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
     pokeCard.className = 'card'
-    let cardFront = document.createElement('div')
-    let frontLabel = document.createElement('p')
-    let frontImage = document.createElement('img')
-    let cardBack = document.createElement('div')
-    let backLabel = document.createElement('p')
-
-
-    frontLabel.textContent = pokemon.name
-    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
-    backLabel.textContent = `I'm the back of the card`
-    cardFront.appendChild(frontImage)
-    cardFront.appendChild(frontLabel)
-    cardBack.appendChild(backLabel)
-    pokeCard.appendChild(cardFront)
-    pokeCard.appendChild(cardBack)
+    pokeCard.addEventListener('click', () => {
+        pokeCard.classList.toggle('is-flipped')
+    })
+    
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeCard.appendChild(populateCardBack(pokemon))
     pokeScene.appendChild(pokeCard)
     pokemonGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    let cardFront = document.createElement('div')
+    cardFront.className = `card__face card__face--front`
+    let frontLabel = document.createElement('p')
+    let frontImage = document.createElement('img')
+    frontLabel.textContent = pokemon.name
+    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
+    cardFront.appendChild(frontImage)
+    cardFront.appendChild(frontLabel)
+    return cardFront
+}
+
+function populateCardBack(pokemon) {
+    let cardBack = document.createElement('div')
+    cardBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `I'm the back of the card`
+    cardBack.appendChild(backLabel)
+    return cardBack
 }
 
 function getImageFileName(pokemon) {
@@ -56,4 +90,13 @@ function getImageFileName(pokemon) {
     }
 }
 
-loadPage()
+function Pokemon(name, height, weight, abilities) {
+    this.name = name
+    this.height = height
+    this.weight = weight
+    this.abilities = abilities
+    this.id = 900
+}
+
+let thoremon = new Pokemon('Thoremon', 450, 200, ['gorge', 'sleep'])
+console.log(thoremon)
